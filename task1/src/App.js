cfe7a214d67bb3b33cee4f2e2aa716fff75b80f3
shivanipolichetti <shivanipolichetti2@gmail.com>
2024-04-01 23:25:18 +0530

    day1

---
+++ task1/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <center><h1>P.SHIVANI</h1></center>
+        <p><b>Email:</b>shivanipolichetti2@gmail.com</p>
+        <p><b>Phone no.:</b>8143399545</p>
+        <h2>Career Objective:</h2>
+        <p>To work in reputable company to enhance my skills and expand company's growth.</p>
+        <h2>Qualifications:</h2>
+        <table border="2">
+          <tr>
+            <th>S.No.</th>
+            <th>Education</th>
+            <th>Passout Year</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>B.tech(ECE)</td>
+            <td>2023</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Inter(MPC)</td>
+            <td>2019</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>10th(SSC)</td>
+            <td>2017</td>
+          </tr>
+
+        </table>
+        <h2>Technical Skills:</h2>
+        <ul>
+          <li>C</li>
+          <li>Python</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+        <h2>Declaration:</h2>
+        <p>I declared that all above details about me are correct.</p>
+      </div>
+  );
+}
+
+export default App;
